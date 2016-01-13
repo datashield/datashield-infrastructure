33a0cb157437437a72b3be8f806be202b1f78037
Neil Parley <mail@neilparley.co.uk>
2016-01-13 17:22:59 +0000

    First commit

---
+++ scripts/centos/setup.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+PUPPET_DIR="/opt/puppetlabs"
+PUPPET_BIN="$PUPPET_DIR/puppet/bin"
+
+DISTRIB_RELEASE=`rpm -qa '*release*' --queryformat '%{VERSION}'`
+
+echo "Running Puppet setup script for Centos"
+echo "Centos Enterprise version: $DISTRIB_RELEASE"
+
+# Install Puppet
+if [ ! -d "$PUPPET_DIR" ]; then
+    echo "Installing Puppet"
+    sudo rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-${DISTRIB_RELEASE}.noarch.rpm
+    sudo yum update -y
+    sudo yum install puppet-agent -y
+fi
+# Install GIT
+sudo yum install git -y
+# Install r10k
+echo "Checking / Installing r10k"
+${PUPPET_BIN}/gem query --name r10k --installed &> /dev/null || sudo ${PUPPET_BIN}/gem install r10k
+
+if [ ! -e "$PUPPET_DIR/bin/r10k" ]; then
+    ln -s ${PUPPET_BIN}/r10k ${PUPPET_DIR}/bin/r10k
+fi
---
+++ scripts/ubuntu/setup.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+PUPPET_DIR="/opt/puppetlabs"
+PUPPET_BIN="$PUPPET_DIR/puppet/bin"
+
+DISTRIB_CODENAME=`lsb_release -sc`
+
+echo "Running Puppet setup script for Ubuntu"
+echo "Ubuntu codename: $DISTRIB_CODENAME"
+
+# Install Puppet
+if [ ! -d "$PUPPET_DIR" ]; then
+    echo "Installing Puppet"
+    wget -nv https://apt.puppetlabs.com/puppetlabs-release-pc1-${DISTRIB_CODENAME}.deb
+    sudo dpkg -i puppetlabs-release-pc1-${DISTRIB_CODENAME}.deb
+    sudo rm -f puppetlabs-release-pc1-${DISTRIB_CODENAME}.deb
+    sudo apt-get update
+    sudo apt-get install puppet-agent -y
+fi
+# Install GIT
+sudo apt-get install git -y
+# Install r10k
+echo "Checking / Installing r10k"
+${PUPPET_BIN}/gem query --name r10k --installed &> /dev/null || sudo ${PUPPET_BIN}/gem install r10k
+
+if [ ! -e "$PUPPET_DIR/bin/r10k" ]; then
+    ln -s ${PUPPET_BIN}/r10k ${PUPPET_DIR}/bin/r10k
+fi
