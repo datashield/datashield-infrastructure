5f7bd80c08b5c86340f56288d9abf5b2a95cca96
Neil Parley <mail@neilparley.co.uk>
2016-01-19 14:44:42 +0000

    Create an example where the opal password and password hash are changed when including the datashield package

--- puppet/environments/test/manifests/site.pp
+++ puppet/environments/test/manifests/site.pp
@@ -1,1 +1,10 @@
-include datashield
+case $::operatingsystem {
+  'Ubuntu': {
+    $opal_password_hash = '$shiro1$SHA-256$500000$gcnVxdEmOjaN+NfsK/1NsA==$UOobbhJsBBojnbsfzIBX9GTWjWQFi8aJZxFvFKmOiSE='
+  }
+  'Centos': {
+    $opal_password_hash = '$shiro1$SHA-256$500000$5Zx3jiOtGFLYYqboKSgFgQ==$qUiuEUIMsEfVash4nqtr8A94/GD6B8Kdlv8bll3wg2M='
+  }
+}
+
+class {::datashield: opal_password_hash => $opal_password_hash, opal_password => 'datashield_test&'}
