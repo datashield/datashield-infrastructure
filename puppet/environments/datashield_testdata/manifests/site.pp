4445e4dea5b4a45a6b906218e65d5bed86c9d1c3
Neil Parley <mail@neilparley.co.uk>
2016-02-16 16:28:53 +0000

    Update site.pp for datashield_testdata

--- puppet/environments/datashield_testdata/manifests/site.pp
+++ puppet/environments/datashield_testdata/manifests/site.pp
@@ -1,3 +1,8 @@
+# Datashield server with test data.
+#
+# Install mysql and mongodb, include test data and install the firewall. Update opal admin password
+#
+
 case $::operatingsystem {
   'Ubuntu': {
     $opal_password_hash = '$shiro1$SHA-256$500000$gcnVxdEmOjaN+NfsK/1NsA==$UOobbhJsBBojnbsfzIBX9GTWjWQFi8aJZxFvFKmOiSE='
@@ -7,4 +12,13 @@ case $::operatingsystem {
   }
 }
 
-class {::datashield: opal_password_hash => $opal_password_hash, opal_password => 'datashield_test&'}
+class { ::datashield:
+  test_data          => true,
+  firewall           => true,
+  mysql              => true,
+  mongodb            => true,
+  remote_mongodb     => false,
+  remote_mysql       => false,
+  opal_password_hash => $opal_password_hash,
+  opal_password      => 'datashield_test&'
+}
