1eab9d207341ddb5a301949a562c4645d5cb8d7e
Neil Parley <mail@neilparley.co.uk>
2016-02-16 16:48:47 +0000

    Update client site.pp

--- puppet/environments/datashield_client/manifests/site.pp
+++ puppet/environments/datashield_client/manifests/site.pp
@@ -1,1 +1,13 @@
-include ::datashield::client
+# Datashield client server
+#
+# Install R and datashield client packages. Install Rstudio and set up default user for login.
+# This is an example please change to meet the needs of your install. Consider changing any passwords in this file!
+#
+
+class { ::datashield::client:
+  rstudio       => true,                # Install rstudio on the machine
+  firewall      => true,                # Install the firewall on the machine
+  create_user   => true,                # Create a default user account with rstudio install
+  user_name     => 'datashield',        # Name of user to create
+  password_hash => 'mrtyHtvJlH8D2'      # Password hash of user to create
+}
