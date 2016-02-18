40d5e24952a2864a24e1179a690d8be4ae56e3a1
Neil Parley <mail@neilparley.co.uk>
2016-02-18 12:18:28 +0000

    Add a puppet datashield remote db environment

---
+++ puppet/environments/datashield_remotedb/manifests/site.pp
@@ -0,0 +1,32 @@
+# Datashield server without data and which connects to remote database servers.
+#
+# Does not install mysql server or a mongodb server or test data but does install the firewall. Update opal
+# admin password. Registers remote db server(s) with Opal.
+# This is an example please change to meet the needs of your install. Consider changing any passwords in this file!
+#
+
+case $::operatingsystem {
+  'Ubuntu': {
+    $opal_password_hash = '$shiro1$SHA-256$500000$gcnVxdEmOjaN+NfsK/1NsA==$UOobbhJsBBojnbsfzIBX9GTWjWQFi8aJZxFvFKmOiSE='
+  }
+  'Centos': {
+    $opal_password_hash = '$shiro1$SHA-256$500000$5Zx3jiOtGFLYYqboKSgFgQ==$qUiuEUIMsEfVash4nqtr8A94/GD6B8Kdlv8bll3wg2M='
+  }
+}
+
+class { ::datashield:
+  test_data           => false,                  # No test data
+  firewall            => true,                   # Install firewall on server and open ports
+  mysql               => false,                  # Do not install mysql server
+  mongodb             => false,                  # Do not install mongodb server
+  remote_mongodb      => true,                   # Connect to remote mongodb server (or set to false if no mongodb server)
+  remote_mongodb_url  => 'mongodb.server.domain',# Remote mongodb server url
+  remote_mongodb_user => 'username',             # Username for remote mongodb server
+  remote_mongodb_pass => 'password',             # Password for remote mongodb server
+  remote_mysql        => true,                   # Connect to remote mysql server (or set to false if no mysql server)
+  remote_mysql_url    => 'mysql.server.domain',  # Remote mysql server url
+  remote_mysql_user   => 'username',             # Username for remote mysql server
+  remote_mysql_pass   => 'password',             # Password for remote mysql server
+  opal_password_hash  => $opal_password_hash,    # Change Opal admin password using this hash
+  opal_password       => 'datashield_test&'      # This is the Opal admin password for Opal management
+}
