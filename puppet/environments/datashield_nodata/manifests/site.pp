d93cb3c5a9946f3d0bff617e15b21750195c5857
Neil Parley <mail@neilparley.co.uk>
2016-02-22 13:38:22 +0000

    Add MySQL and MongoDB username and passwords into the site.pp files.

--- puppet/environments/datashield_nodata/manifests/site.pp
+++ puppet/environments/datashield_nodata/manifests/site.pp
@@ -14,12 +14,17 @@ case $::operatingsystem {
 }
 
 class { ::datashield:
-  test_data          => false,                  # No test data
-  firewall           => true,                   # Install firewall on server and open ports
-  mysql              => true,                   # Install mysql server
-  mongodb            => true,                   # Install mongodb server
-  remote_mongodb     => false,                  # No remote mongodb servers
-  remote_mysql       => false,                  # No remote mysql servers
-  opal_password_hash => $opal_password_hash,    # Change Opal admin password using this hash
-  opal_password      => 'datashield_test&'      # This is the Opal admin password for Opal management
+  test_data           => false,                  # No test data
+  firewall            => true,                   # Install firewall on server and open ports
+  mysql               => true,                   # Install mysql server
+  mysql_root_password => 'rootpass',             # Root password for MySQL install
+  mysql_user          => 'opaluser',             # MySQL user for Opal table
+  mysql_pass          => 'opalpass',             # MySQL user passport for Opal table
+  mongodb             => true,                   # Install mongodb server
+  mongodb_user        => 'opaluser',             # Username of root MongoDB user for MongoDB install
+  mongodb_pass        => 'opalpass',             # Password of root MongoDB user for MongoDB install
+  remote_mongodb      => false,                  # No remote mongodb servers
+  remote_mysql        => false,                  # No remote mysql servers
+  opal_password_hash  => $opal_password_hash,    # Change Opal admin password using this hash
+  opal_password       => 'datashield_test&'      # This is the Opal admin password for Opal management
 }
