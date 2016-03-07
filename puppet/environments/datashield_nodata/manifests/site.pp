33d8f175257ee0d7f0d2dda06e94a1364a8dc9b4
Neil Parley <mail@neilparley.co.uk>
2016-03-07 12:22:31 +0000

    Add opal_release to the example site.pp files

--- puppet/environments/datashield_nodata/manifests/site.pp
+++ puppet/environments/datashield_nodata/manifests/site.pp
@@ -26,5 +26,6 @@ class { ::datashield:
   remote_mongodb      => false,                  # No remote mongodb servers
   remote_mysql        => false,                  # No remote mysql servers
   opal_password_hash  => $opal_password_hash,    # Change Opal admin password using this hash
-  opal_password       => 'datashield_test&'      # This is the Opal admin password for Opal management
+  opal_password       => 'datashield_test&',     # This is the Opal admin password for Opal management
+  opal_release        => 'stable'                # The release version of Opal to install.
 }
