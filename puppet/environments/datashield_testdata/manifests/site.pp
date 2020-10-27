b396840e4d8c39628d735ffdbd544e9278d9643f
Stuart Wheater <stuart.wheater@arjuna.com>
2020-10-27 17:22:24 +0000

    Switch to v6.2-dev

--- puppet/environments/datashield_testdata/manifests/site.pp
+++ puppet/environments/datashield_testdata/manifests/site.pp
@@ -30,7 +30,7 @@ class { ::datashield:
   opal_release         => 'stable',               # The release version of Opal to install.
 
   dsbase_githubusername      => 'datashield',
-  dsbase_ref                 => '6.1',
+  dsbase_ref                 => 'v6.2-dev',
   dsdanger_githubusername    => 'datashield',
   dsdanger_ref               => ''
 }
