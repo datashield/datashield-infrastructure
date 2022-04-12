c43799dce6346dfd848ad0689f694aaa2bef4f69
Stuart Wheater <stuart.wheater@arjuna.com>
2022-04-12 17:56:11 +0100

    Setup to be 'master'

--- puppet/environments/datashield_testdata/manifests/site.pp
+++ puppet/environments/datashield_testdata/manifests/site.pp
@@ -30,7 +30,7 @@ class { ::datashield:
   opal_release         => 'stable',               # The release version of Opal to install.
 
   dsbase_githubusername      => 'datashield',
-  dsbase_ref                 => '6.2',
+  dsbase_ref                 => 'master',
   dsdanger_githubusername    => 'datashield',
   dsdanger_ref               => ''
 }
